1f97d10df30a5c6769cdb2dc5c270f28eed0d0a4
Serghei Madan <Serghei.Madan@esintl.com>
2020-01-29 10:44:50 -0800

    day 2 problem 1

---
+++ rotate-array.js
@@ -0,0 +1,47 @@
+// Array
+
+// Easy
+
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+// Example 1:
+
+// Input: [1,2,3,4,5,6,7] and k = 3
+// Output: [5,6,7,1,2,3,4]
+// Explanation:
+// rotate 1 steps to the right: [7,1,2,3,4,5,6]
+// rotate 2 steps to the right: [6,7,1,2,3,4,5]
+// rotate 3 steps to the right: [5,6,7,1,2,3,4]
+// Example 2:
+
+// Input: [-1,-100,3,99] and k = 2
+// Output: [3,99,-1,-100]
+// Explanation: 
+// rotate 1 steps to the right: [99,-1,-100,3]
+// rotate 2 steps to the right: [3,99,-1,-100]
+
+
+// Solution 1:
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    if (k > 0) {
+      reverse(nums, 0, nums.length - 1);
+      reverse(nums, 0, k - 1);
+      reverse(nums, k, nums.length - 1);
+    }
+};
+  
+  const reverse = (arr, start, end) => {
+    if (!arr || !arr.length || start >= end) return;
+    while (start < end) {
+      [arr[start], arr[end]] = [arr[end], arr[start]];
+      start++;
+      end--;
+    }
+};
