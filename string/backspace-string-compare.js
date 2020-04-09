5341a3cd531f9409ff97437a947ff5ab307e74b4
truelav <madan.serghei@yahoo.com>
2020-04-09 11:47:07 -0700

    daily challenge string easy

---
+++ string/backspace-string-compare.js
@@ -0,0 +1,57 @@
+// Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+
+// Example 1:
+
+// Input: S = "ab#c", T = "ad#c"
+// Output: true
+// Explanation: Both S and T become "ac".
+// Example 2:
+
+// Input: S = "ab##", T = "c#d#"
+// Output: true
+// Explanation: Both S and T become "".
+// Example 3:
+
+// Input: S = "a##c", T = "#a#c"
+// Output: true
+// Explanation: Both S and T become "c".
+// Example 4:
+
+// Input: S = "a#c", T = "b"
+// Output: false
+// Explanation: S becomes "c" while T becomes "b".
+// Note:
+
+// 1 <= S.length <= 200
+// 1 <= T.length <= 200
+// S and T only contain lowercase letters and '#' characters.
+// Follow up:
+
+// Can you solve it in O(N) time and O(1) space?
+
+
+var backspaceCompare = function(S, T) {
+    const stack = [];
+    const stack1 = [];
+    
+    for(let i = 0; i < S.length; i++){
+        if (S[i] !== '#'){
+            stack.push(S[i])
+        } 
+        if (S[i] === '#'){
+            stack.pop()
+        }
+    }
+    
+    for(let i = 0; i < T.length; i++){
+        if (T[i] !== '#'){
+            stack1.push(T[i])
+        } 
+        if (T[i] === '#'){
+            stack1.pop()
+        }
+
+    }
+    
+    return JSON.stringify(stack) == JSON.stringify(stack1)
+};
