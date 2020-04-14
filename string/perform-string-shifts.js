b8dc7213bee6c7cfbd9af0b62c9b991f923294dd
truelav <madan.serghei@yahoo.com>
2020-04-14 11:13:16 -0700

    daily code challenge string medium

---
+++ string/perform-string-shifts.js
@@ -0,0 +1,77 @@
+// You are given a string s containing lowercase English letters, and a matrix shift, where shift[i] = [direction, amount]:
+
+// direction can be 0 (for left shift) or 1 (for right shift). 
+// amount is the amount by which string s is to be shifted.
+// A left shift by 1 means remove the first character of s and append it to the end.
+// Similarly, a right shift by 1 means remove the last character of s and add it to the beginning.
+// Return the final string after all operations.
+
+ 
+
+// Example 1:
+
+// Input: s = "abc", shift = [[0,1],[1,2]]
+// Output: "cab"
+// Explanation: 
+// [0,1] means shift to left by 1. "abc" -> "bca"
+// [1,2] means shift to right by 2. "bca" -> "cab"
+// Example 2:
+
+// Input: s = "abcdefg", shift = [[1,1],[1,1],[0,2],[1,3]]
+// Output: "efgabcd"
+// Explanation:  
+// [1,1] means shift to right by 1. "abcdefg" -> "gabcdef"
+// [1,1] means shift to right by 1. "gabcdef" -> "fgabcde"
+// [0,2] means shift to left by 2. "fgabcde" -> "abcdefg"
+// [1,3] means shift to right by 3. "abcdefg" -> "efgabcd"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 100
+// s only contains lower case English letters.
+// 1 <= shift.length <= 100
+// shift[i].length == 2
+// 0 <= shift[i][0] <= 1
+// 0 <= shift[i][1] <= 100
+
+
+var stringShift = function(s, shift) {
+    
+    let result = '';
+    let stack = 0;
+    let leng = s.length
+    
+    for (let i = 0; i < shift.length; i++){
+        (shift[i][0] === 0) ? stack -= shift[i][1] : stack += shift[i][1] 
+    }
+    
+    console.log(stack)
+
+    if (stack < 0){
+        if (Math.abs(stack) > leng){
+            stack = Math.abs(stack)
+            while(stack > leng){
+                stack -= leng
+            }
+        }
+        let moveToEnd =  s.substring(0, Math.abs(stack))
+        let rest = s.substring( Math.abs(stack))
+        result += rest + moveToEnd
+    } else if (stack > 0){
+        if (Math.abs(stack) > leng){
+            stack = Math.abs(stack)
+            while(stack > leng){
+                stack -= leng
+            }
+        }
+        let rest =  s.substring(0, leng - stack)
+        let moveToBegin = s.substring(leng - stack)
+        result += moveToBegin+ rest
+    }
+    
+
+    
+    return result
+    
+};
