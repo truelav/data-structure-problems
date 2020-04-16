4ff21d31aaba495cc9f953fd2d4589eace3d778e
truelav <madan.serghei@yahoo.com>
2020-04-16 13:49:26 -0700

    string easy maximum 69 number

---
+++ string/maximum-69-number.js
@@ -0,0 +1,48 @@
+// Given a positive integer num consisting only of digits 6 and 9.
+
+// Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+
+ 
+
+// Example 1:
+
+// Input: num = 9669
+// Output: 9969
+// Explanation: 
+// Changing the first digit results in 6669.
+// Changing the second digit results in 9969.
+// Changing the third digit results in 9699.
+// Changing the fourth digit results in 9666. 
+// The maximum number is 9969.
+// Example 2:
+
+// Input: num = 9996
+// Output: 9999
+// Explanation: Changing the last digit 6 to 9 results in the maximum number.
+// Example 3:
+
+// Input: num = 9999
+// Output: 9999
+// Explanation: It is better not to apply any change.
+
+
+var maximum69Number  = function(num) {
+    
+    let str = '' + num;
+    
+    for (let i = 0; i < str.length; i++){
+        if (str[i] === '6'){
+           if(i === 0){
+               str = 9 + str.substring(1)
+           } else if (i === str.length - 1){
+             str = str.substring(0, str.length - 1) + 9  
+           } else {
+               str = str.substring(0, i) + 9 + str.substring(i + 1)
+           } 
+            break
+        }
+    }
+    
+   return parseInt(str, 10)
+    
+};
