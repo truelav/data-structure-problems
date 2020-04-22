5cc6cbf69de8f419832686cb983a8c7ca6163411
truelav <madan.serghei@yahoo.com>
2020-04-22 10:52:42 -0700

    daily challenge array medium brute force

---
+++ array/medium/subarray-sum-equals-k.js
@@ -0,0 +1,34 @@
+// Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+// Example 1:
+// Input:nums = [1,1,1], k = 2
+// Output: 2
+// Note:
+// The length of the array is in range [1, 20,000].
+// The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+
+// Approach 1  Brute force
+
+var subarraySum = function(nums, k) {
+    
+    let sum = 0;
+    let result = 0;
+    
+    for (let  start = 0; start < nums.length; start++){
+        for (let end= start + 1; end <= nums.length; end++){
+            let sum = 0;
+            for (let i = start; i < end; i++ ){
+                sum += nums[i]
+                if (sum === k){
+                    result++
+                }
+            }
+        }
+    }
+    
+    return result;
+};
+
+
+// Aproach 2 Hash
+
