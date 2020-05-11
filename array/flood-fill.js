409db30f064851ade0ee8fecc26963f6be66ca6f
truelav <madan.serghei@yahoo.com>
2020-05-11 11:04:05 -0700

    daily challenge array easy fill flood

---
+++ array/flood-fill.js
@@ -0,0 +1,60 @@
+// An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+
+// Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+
+// To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+
+// At the end, return the modified image.
+
+// Example 1:
+// Input: 
+// image = [[1,1,1],[1,1,0],[1,0,1]]
+// sr = 1, sc = 1, newColor = 2
+// Output: [[2,2,2],[2,2,0],[2,0,1]]
+// Explanation: 
+// From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected 
+// by a path of the same color as the starting pixel are colored with the new color.
+// Note the bottom corner is not colored 2, because it is not 4-directionally connected
+// to the starting pixel.
+// Note:
+
+// The length of image and image[0] will be in the range [1, 50].
+// The given starting pixel will satisfy 0 <= sr < image.length and 0 <= sc < image[0].length.
+// The value of each color in image[i][j] and newColor will be an integer in [0, 65535].
+
+// Using a stack to check all Adjacent pixels to the start and end pixel then change the color
+
+var floodFill = function(image, sr, sc, newColor) {
+    
+    const currentColor = image[sr][sc];
+    
+    if (newColor === currentColor){
+        return image
+    }
+    
+    const rowLength = image.length - 1;
+    const colLength = image[0].length - 1;
+    
+    let stack = [[sr, sc]];
+    
+    while (stack.length !== 0){
+        let curr = stack.pop();
+        let [row, col] = curr;
+        
+        if (row > 0 && image[row - 1][col] === currentColor)
+            stack.push([row - 1, col]);
+        if (row < rowLength && image[row+1][col] === currentColor)
+            stack.push([row+1, col]);
+        if (col > 0 && image[row][col-1] === currentColor)
+            stack.push([row, col-1]);
+        if (col < colLength && image[row][col+1] === currentColor)
+            stack.push([row, col+1]);
+        
+        // Set the color
+        image[row][col] = newColor;
+        
+    }
+    
+    return image
+    
+};
