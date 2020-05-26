e600c26db08f53509630fcbbd42a37778d408c52
unknown <madan.serghei@yahoo.com>
2020-05-26 10:39:06 -0700

    daily challenge esy reverse tree

---
+++ google/tree/invert-binary-tree.js
@@ -0,0 +1,58 @@
+// 226. Invert Binary Tree
+// Easy
+
+// 2944
+
+// 44
+
+// Add to List
+
+// Share
+// Invert a binary tree.
+
+// Example:
+
+// Input:
+
+//      4
+//    /   \
+//   2     7
+//  / \   / \
+// 1   3 6   9
+// Output:
+
+//      4
+//    /   \
+//   7     2
+//  / \   / \
+// 9   6 3   1
+// Trivia:
+// This problem was inspired by this original tweet by Max Howell:
+
+// Google: 90% of our engineers use the software you wrote (Homebrew), but you can’t invert a binary tree on a whiteboard so f*** off.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) return root;
+    visit(root);
+    return root;
+};
+
+function visit(node) {
+    const temp = node.left;
+    node.left = node.right;
+    node.right = temp;
+    if (node.left) visit(node.left);
+    if (node.right) visit(node.right);
+}
